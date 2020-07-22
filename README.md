1d5289f526ba6677e854c835be893f29782aa5e7
Tony Rosler <tony.rosler246@gmail.com>
2020-07-22 13:18:53 +0200

    add ignore stuff. User sketch, upload and base are working.  User sketch needs small changes with clicking event

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 ## myFourierEpicycles - you give me an image I give you its epicycles.
 
-This is the code for a website I'm creating myFourierEpicyles.com. The site allows users
+![]()
+
+This is the code for a website I'm creating myFourierEpicyles.com. The site has the following functionality:
 
 - Upload an image and find the its fourier epicycles.
 - Draw in an image and find its fourier epicycles.
 
-![]()
+Something which makes this project differnet than other fourier epicycles websites, is that it allows users to upload any image of a variaty of forms (png, jpeg, svg, gif) and then.
 
-This code allows users to upload any image (png, jpeg, svg, gif) and the fourier epicycles drawing is.
+In order to find the arbitrary path of any image type in a efficent way, I created a new api which can be found [here](https://github.com/trozler/ImageToPoints).
 
 ### Important notes
 
-- For **best drawing** upload **svg** images with **closed** paths. This is the preferred method.
-- I have had to alter the source code of almost every node dependecy, as such the node_modules folder is included in repository.
+- For **most clear drawings** upload **svg** images with **closed** paths.
+- I have had to alter the source code of almost every dependecy, as such the node_modules folder is included in repository.
 
 ### How to install
 
@@ -20,12 +22,12 @@ This code allows users to upload any image (png, jpeg, svg, gif) and the fourier
 
 ### Understanding Fourier Transfrom
 
-For anyone who is curious these resources are good starting places in understanding the fourier transform and the drawing of epicycles.
+For those curious, these resources are good starting points in understanding the fourier transform and the drawing of epicycles.
 
 3Blue1Brown (amazing explination):
 https://www.youtube.com/watch?v=spUNpyF58BY
 
-More Great Visusals:
+More great visusals:
 http://www.jezzamon.com/fourier/index.html
 
 The Coding Train (create your own epicycles):
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-// import { userSketch } from "./sketchUser.js";
-// import { logoSketch } from "./sketchLogo.js";
 import { pathfinderImage, pathfinderSVG } from "./findPath.js";
 import { dft, realFFT } from "./fourier.js";
-import { logoDrawing } from "./testpaths/codingtrain2.js";
+
 import { epiCycles } from "./epicycles.js";
 
 //Upload png here.
@@ -53,7 +51,7 @@ Dropzone.options.myDropzone = {
   },
 };
 
-const n_points = 1000;
+const n_points = 800;
 
 function mainPathFinder(image, svgBool) {
   if (svgBool) {
@@ -76,7 +74,10 @@ function myhandler(arr) {
     var pathSketch = arr;
 
     p5.setup = function () {
-      p5.createCanvas(window.innerWidth, window.innerHeight);
+      let cnv = p5.createCanvas(700, 600);
+      cnv.parent("upload-sketch");
+      p5.frameRate(25);
+
       const skip = 2; //As we have too many values can skip some points.
       //Push path into x and y arrays.
       for (let i = 0; i < pathSketch.length; i += skip) {
@@ -86,9 +87,17 @@ function myhandler(arr) {
       //TODO: Add scale for users to dictate how many epicycles.
       const scale = 0.9; //A number in the interval (0, 1].
       const minAmplitude = 0.01;
+      const maxAmplitude = 120;
 
-      fourierX = dft(p5, x).filter((f) => f.amp > minAmplitude);
-      fourierY = dft(p5, y).filter((f) => f.amp > minAmplitude);
+      //TODO: USer tinkering,
+      //If circles touch outside area don't include. Use size of window.
+
+      fourierX = dft(p5, x).filter(
+        (f) => f.amp > minAmplitude && f.amp < maxAmplitude
+      );
+      fourierY = dft(p5, y).filter(
+        (f) => f.amp > minAmplitude && f.amp < maxAmplitude
+      );
 
       //The cycles are being drawn in order of frequency. e.g. C0, C1, ... Where C1 has a frequency. Recall frequency is k and we are calulating Ck.
 
@@ -101,18 +110,10 @@ function myhandler(arr) {
 
     //Draws pictuer.
     p5.draw = function () {
-      p5.background(47, 72, 88);
+      p5.background(255, 255, 255);
 
-      //Create the 2 epicycle generaters.
-      let vx = epiCycles(p5, time, p5.width / 2 - 230, 100, 0, fourierX); //Vector x
-      let vy = epiCycles(
-        p5,
-        time,
-        200,
-        p5.height / 2 - 230,
-        p5.HALF_PI,
-        fourierY
-      ); //Vector y.
+      let vx = epiCycles(p5, time, 300, 450, 0, fourierX, false); //Vector x
+      let vy = epiCycles(p5, time, 500, 200, p5.HALF_PI, fourierY, false); //Vector y.
       let v = p5.createVector(vx.x, vy.y); //As we want coordinate from fourierX and y from fourierY.
       path.push(v);
       p5.line(vx.x, vx.y, v.x, v.y); //Draw the x and y of drawing.
@@ -138,96 +139,107 @@ function myhandler(arr) {
     };
   };
 
-  let userSketch = function (p5) {
-    const USER = 0;
-    const FOURIER = 1;
-    //2 states eitehr we are taking user input or we completing a fourier transform.
+  let logoP5 = new p5(logoSketch);
+}
 
-    let x = [];
-    let y = [];
-    let fourierX = [];
-    let fourierY = [];
-    let time = 0;
-    let path = [];
-    let drawing = [];
-    let state = -1; //To begin width state is negative one i.e. not in user or fft.
+let userSketch = function (p5) {
+  const USER = 0;
+  const FOURIER = 1;
+  //2 states eitehr we are taking user input or we completing a fourier transform.
 
-    p5.mousePressed = function () {
-      state = USER; //Click mouse state user.
-      drawing = [];
-      x = [];
-      y = [];
-      time = 0;
-      path = [];
-    };
+  let x = [];
+  let y = [];
+  let fourierX = [];
+  let fourierY = [];
+  let time = 0;
+  let path = [];
+  let drawing = [];
+  let state = -1; //To begin width state is negative one i.e. not in user or fft.
 
-    p5.mouseReleased = function () {
-      state = FOURIER; //release mouse state fourier.
-      const skip = 1; //Skip every other point.
-      for (let i = 0; i < drawing.length; i += skip) {
-        x.push(drawing[i].x);
-        y.push(drawing[i].y);
-      }
-      //TODO: Add scale for users to dictate how many epicycles.
-      const scale = 1; //A number in the interval (0, 1].
-
-      fourierX = dft(p5, x);
-      fourierY = dft(p5, y);
-
-      fourierX = fourierX.slice(0, Math.floor(scale * fourierX.length));
-      fourierY = fourierY.slice(0, Math.floor(scale * fourierY.length));
-
-      fourierX.sort((a, b) => b.amp - a.amp);
-      fourierY.sort((a, b) => b.amp - a.amp);
-    };
-
-    p5.setup = function () {
-      p5.createCanvas(window.innerWidth, window.innerHeight);
-    };
-
-    p5.draw = function () {
-      p5.background(169, 188, 208);
-
-      if (state == USER) {
-        //If drawing need to record the points relative to window. i.e. center.
-        let point = p5.createVector(
-          p5.mouseX - p5.width / 2,
-          p5.mouseY - p5.height / 2
-        );
-        drawing.push(point); //Push point to drawing.
-        p5.stroke(255);
-        p5.noFill();
-        p5.beginShape(); //Render what the user is drawing.
-        for (let p of drawing) {
-          p5.vertex(p.x - 200 + p5.width / 2, p.y - 200 + p5.height / 2); //When we draw we want points to be relative to center. Not 0, 0 topleft.
-        }
-        p5.endShape();
-      } else if (state == FOURIER) {
-        let vx = epiCycles(p5, time, p5.width / 2, 100, 0, fourierX);
-        let vy = epiCycles(p5, time, 100, p5.height / 2, p5.HALF_PI, fourierY);
-        let v = p5.createVector(vx.x, vy.y);
-        path.push(v);
-        p5.line(vx.x, vx.y, v.x, v.y);
-        p5.line(vy.x, vy.y, v.x, v.y);
-
-        p5.beginShape();
-        p5.noFill();
-        for (let i = 0; i < path.length; i++) {
-          p5.vertex(path[i].x, path[i].y);
-        }
-        p5.endShape();
-
-        const dt = p5.TWO_PI / fourierY.length;
-        time += dt;
-
-        if (time > p5.TWO_PI) {
-          time = 0;
-          path = [];
-        }
-      }
-    };
+  p5.mousePressed = function () {
+    state = USER; //Click mouse state user.
+    drawing = [];
+    x = [];
+    y = [];
+    time = 0;
+    path = [];
   };
 
-  let logoP5 = new p5(logoSketch, "c1");
-  //let userP5 = new p5(userSketch, "c2");
-}
+  p5.mouseReleased = function () {
+    state = FOURIER; //release mouse state fourier.
+    const skip = 1; //Skip every other point.
+    for (let i = 0; i < drawing.length; i += skip) {
+      x.push(drawing[i].x);
+      y.push(drawing[i].y);
+    }
+    //TODO: Add scale for users to dictate how many epicycles.
+    const scale = 1; //A number in the interval (0, 1].
+
+    fourierX = dft(p5, x);
+    fourierY = dft(p5, y);
+
+    fourierX = fourierX.slice(0, Math.floor(scale * fourierX.length));
+    fourierY = fourierY.slice(0, Math.floor(scale * fourierY.length));
+
+    fourierX.sort((a, b) => b.amp - a.amp);
+    fourierY.sort((a, b) => b.amp - a.amp);
+  };
+
+  p5.setup = function () {
+    let cnv = p5.createCanvas(700, 600);
+    cnv.parent("draw-yourself");
+    p5.frameRate(25);
+  };
+
+  p5.draw = function () {
+    p5.background(47, 72, 88);
+
+    if (state == USER) {
+      //If drawing need to record the points relative to window. i.e. center.
+      let point = p5.createVector(
+        p5.mouseX - p5.width / 2,
+        p5.mouseY - p5.height / 2
+      );
+      drawing.push(point); //Push point to drawing.
+      p5.stroke(255);
+      p5.noFill();
+      p5.beginShape(); //Render what the user is drawing.
+      for (let p of drawing) {
+        p5.vertex(p.x + p5.width / 2, p.y + p5.height / 2); //When we draw we want points to be relative to center. Not 0, 0 topleft.
+      }
+      p5.endShape();
+    } else if (state == FOURIER) {
+      let vx = epiCycles(p5, time, p5.width / 2, 100, 0, fourierX, true);
+      let vy = epiCycles(
+        p5,
+        time,
+        100,
+        p5.height / 2,
+        p5.HALF_PI,
+        fourierY,
+        true
+      );
+      let v = p5.createVector(vx.x, vy.y);
+      path.push(v);
+      p5.line(vx.x, vx.y, v.x, v.y);
+      p5.line(vy.x, vy.y, v.x, v.y);
+
+      p5.beginShape();
+      p5.noFill();
+      for (let i = 0; i < path.length; i++) {
+        p5.vertex(path[i].x, path[i].y);
+      }
+      p5.endShape();
+
+      const dt = p5.TWO_PI / fourierY.length;
+      time += dt;
+
+      if (time > p5.TWO_PI) {
+        time = 0;
+        path = [];
+      }
+    }
+  };
+};
+
+let userP5 = new p5(userSketch);
