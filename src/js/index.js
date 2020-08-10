2c469db6e95cfd1802532125e32117eb9a454488
Tony Rosler <tony.rosler246@gmail.com>
2020-08-10 23:07:22 +0200

    sidebar styled and navigation implemented. Working on ubild now.
    Former-commit-id: 2ffad54c7dd1232fff55cf2496aba14027eff5cb
    Former-commit-id: 50a0bcb163810a1dc89fc926ec9c7624a041e4a2

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,38 @@
-import { pathfinderImage, pathfinderSVG } from "./findPath.js";
-import { dft } from "./fourier.js";
-import { epiCycles } from "./epicycles.js";
-import { wave_circle_sketch } from "./waveCircle.js";
-import { userSketch } from "./userSketch.js";
-import { baseSketch } from "./baseEpi.js";
-
-// External dependencies.
-import * as p5 from "p5";
+import { imageHandler } from "./baseEpi.js";
+import { mainPathFinder } from "./mainSketcher.js";
 import * as Dropzone from "dropzone/dist/min/dropzone.min.js";
 
 //Styling
 // import "../css/style.css"; //Include as want to minify.
 // import "../css/dropzone.css"; //Doesnt work some reason, maybe as already minified?
 
-//Run base sketch.
-let baseP5 = new p5(baseSketch);
+//Images
+import Deer from "../images/Deer.svg";
+import Dog from "../images/Dog.svg";
+import Epl from "../images/epl-icon.svg";
+import Tux from "../images/tux.svg";
 
-var n_uploads = 0;
-var logoP5;
+//Add event listener for each image.
+const imageNames = { deer: Deer, epl: Epl, tux: Tux, dog: Dog };
+
+for (const n in imageNames) {
+  document.getElementById(n).src = imageNames[n];
+
+  document.getElementById(n).addEventListener("click", (e) => {
+    e.preventDefault();
+    if (document.getElementById("baseCanvas") !== null) {
+      removeCanvas("baseCanvas");
+    }
+    imageHandler(n);
+  });
+}
+
+window.addEventListener("load", () => imageHandler("deer"), { once: true });
+
+function removeCanvas(id) {
+  let el = document.getElementById(id);
+  el.parentNode.removeChild(el);
+}
 
 Dropzone.options.myDropzone = {
   addRemoveLinks: true,
@@ -64,149 +79,3 @@ Dropzone.options.myDropzone = {
     });
   },
 };
-
-const factor = 3;
-
-function mainPathFinder(image, svgBool) {
-  if (n_uploads > 0) {
-    logoP5.remove();
-  } else {
-    n_uploads++;
-    let el = document.getElementById("placeholder");
-    el.parentNode.removeChild(el);
-  }
-  if (svgBool) {
-    var arr = pathfinderSVG(image, factor);
-    myhandler(arr);
-  } else {
-    pathfinderImage(image.src, myhandler, factor);
-  }
-}
-
-function myhandler(arr) {
-  let customSketch = function (pFIVE) {
-    var x = [];
-    let y = [];
-    let fourierX = [];
-    let fourierY = [];
-    let allFourierX = [];
-    let allFourierY = [];
-
-    let time = 0;
-    let localTime = 0;
-    let currPath = [[]];
-    let currSVGPath = 0;
-
-    pFIVE.setup = function () {
-      let cnv = pFIVE.createCanvas(700, 600);
-      cnv.parent("upload-sketch");
-      pFIVE.frameRate(20);
-
-      for (let subpath of arr) {
-        const skip = 2;
-        //Push path into x and y arrays.
-        for (let i = 0; i < subpath.length; i += skip) {
-          x.push(subpath[i].x);
-          y.push(subpath[i].y);
-        }
-        //TODO: Add scale for users to dictate how many epicycles.
-        const scale = 0.9; //A number in the interval (0, 1].
-        const minAmplitude = 0.01;
-        const maxAmplitude = 120;
-
-        fourierX = dft(pFIVE, x).filter(
-          (f) => f.amp > minAmplitude && f.amp < maxAmplitude
-        );
-        fourierY = dft(pFIVE, y).filter(
-          (f) => f.amp > minAmplitude && f.amp < maxAmplitude
-        );
-
-        //The cycles are being drawn in order of frequency. e.g. C0, C1, ... Where C1 has a frequency. Recall frequency is k and we are calulating Ck.
-
-        fourierX = fourierX.slice(0, Math.floor(scale * fourierX.length));
-        fourierY = fourierY.slice(0, Math.floor(scale * fourierY.length));
-
-        fourierX.sort((a, b) => b.amp - a.amp);
-        fourierY.sort((a, b) => b.amp - a.amp);
-
-        allFourierX.push(fourierX);
-        allFourierY.push(fourierY);
-
-        x.length = 0;
-        y.length = 0;
-      }
-    };
-
-    //Draws pictuer.
-    pFIVE.draw = function () {
-      pFIVE.background(255, 255, 255);
-
-      let vx = epiCycles(
-        pFIVE,
-        localTime,
-        250,
-        500,
-        0,
-        allFourierX[currSVGPath],
-        false
-      ); //Vector x
-      let vy = epiCycles(
-        pFIVE,
-        localTime,
-        550,
-        200,
-        pFIVE.HALF_PI,
-        allFourierY[currSVGPath],
-        false
-      ); //Vector y.
-      let v = pFIVE.createVector(vx.x, vy.y); //As we want coordinate from fourierX and y from fourierY.
-      currPath[currSVGPath].push(v);
-      pFIVE.line(vx.x, vx.y, v.x, v.y); //Draw the x coordinates of drawing.
-      pFIVE.line(vy.x, vy.y, v.x, v.y); //Draw y coordinates of drawing.
-
-      for (let mod = 0; mod < currPath.length; mod++) {
-        //When we redraw points. We need a break when we reach the end of an svg path.
-        pFIVE.beginShape();
-        pFIVE.noFill();
-        //Draw all previouse points of drawing for every frame of animation.
-        for (let i = 0; i < currPath[mod].length; i++) {
-          pFIVE.vertex(currPath[mod][i].x, currPath[mod][i].y);
-        }
-        pFIVE.endShape();
-      }
-
-      const dt = pFIVE.TWO_PI / allFourierY[currSVGPath].length; //Amount of time I move each frame of animatoon.
-      //Should be 2pi a full cycle per frame / the number of fourier coefficents.
-      localTime += dt;
-
-      if (localTime >= pFIVE.TWO_PI) {
-        //Finsihed one svg path completly.
-        time += pFIVE.TWO_PI;
-        // //i.e. We have gone through all svg paths.
-        // if (currSVGPath + 1 === allFourierX.length) {
-        //   //Set to first path, after we spin around twice.
-        //   currSVGPath = 0;
-        // } else {
-        //   currSVGPath++;
-        // }
-        localTime = 0;
-        currSVGPath++;
-        currPath.push([]);
-
-        //After we have drawn all paths and been around once, we reset and draw again.
-        if (time >= pFIVE.TWO_PI * allFourierX.length) {
-          time = 0;
-          currPath.length = 0;
-          currPath.push([]);
-          currSVGPath = 0;
-          localTime = 0;
-        }
-      }
-    };
-  };
-  logoP5 = new p5(customSketch);
-}
-
-var userP5 = new p5(userSketch);
-
-var wave_circlep5 = new p5(wave_circle_sketch);
---
+++ src/js/mainSketcher.js
@@ -0,0 +1,145 @@
+import { pathfinderImage, pathfinderSVG } from "./findPath.js";
+import { dft } from "./fourier.js";
+import { epiCycles } from "./epicycles.js";
+import { wave_circle_sketch } from "./waveCircle.js";
+import { userSketch } from "./userSketch.js";
+
+// External dependencies.
+import * as p5 from "p5";
+
+const factor = 3;
+var n_uploads = 0;
+var logoP5;
+
+export function mainPathFinder(image, svgBool) {
+  if (n_uploads > 0) {
+    logoP5.remove();
+  } else {
+    n_uploads++;
+    let el = document.getElementById("placeholder");
+    el.parentNode.removeChild(el);
+  }
+  if (svgBool) {
+    var arr = pathfinderSVG(image, factor);
+    myhandler(arr);
+  } else {
+    pathfinderImage(image.src, myhandler, factor);
+  }
+}
+
+function myhandler(arr) {
+  let customSketch = function (pFIVE) {
+    var x = [];
+    let y = [];
+    let fourierX = [];
+    let fourierY = [];
+    let allFourierX = [];
+    let allFourierY = [];
+
+    let time = 0;
+    let localTime = 0;
+    let currPath = [[]];
+    let currSVGPath = 0;
+
+    pFIVE.setup = function () {
+      let cnv = pFIVE.createCanvas(700, 600);
+      cnv.parent("upload-sketch");
+      pFIVE.frameRate(20);
+
+      for (let subpath of arr) {
+        const skip = 2;
+        for (let i = 0; i < subpath.length; i += skip) {
+          x.push(subpath[i].x);
+          y.push(subpath[i].y);
+        }
+        //TODO: Add scale for users to dictate how many epicycles.
+        const scale = 0.9; //A number in the interval (0, 1].
+        const minAmplitude = 0.01;
+        const maxAmplitude = 120;
+
+        fourierX = dft(pFIVE, x).filter(
+          (f) => f.amp > minAmplitude && f.amp < maxAmplitude
+        );
+        fourierY = dft(pFIVE, y).filter(
+          (f) => f.amp > minAmplitude && f.amp < maxAmplitude
+        );
+
+        //The cycles are being drawn in order of frequency. e.g. C0, C1, ... Where C1 has a frequency. Recall frequency is k and we are calulating Ck.
+
+        fourierX = fourierX.slice(0, Math.floor(scale * fourierX.length));
+        fourierY = fourierY.slice(0, Math.floor(scale * fourierY.length));
+
+        fourierX.sort((a, b) => b.amp - a.amp);
+        fourierY.sort((a, b) => b.amp - a.amp);
+
+        allFourierX.push(fourierX);
+        allFourierY.push(fourierY);
+
+        x.length = 0;
+        y.length = 0;
+      }
+    };
+    pFIVE.draw = function () {
+      pFIVE.background(255, 255, 255);
+      let vx = epiCycles(
+        pFIVE,
+        localTime,
+        250,
+        500,
+        0,
+        allFourierX[currSVGPath],
+        false
+      );
+      let vy = epiCycles(
+        pFIVE,
+        localTime,
+        550,
+        200,
+        pFIVE.HALF_PI,
+        allFourierY[currSVGPath],
+        false
+      );
+      let v = pFIVE.createVector(vx.x, vy.y); //As we want coordinate from fourierX and y from fourierY.
+      currPath[currSVGPath].push(v);
+      pFIVE.line(vx.x, vx.y, v.x, v.y); //Draw the x coordinates of drawing.
+      pFIVE.line(vy.x, vy.y, v.x, v.y); //Draw y coordinates of drawing.
+
+      for (let mod = 0; mod < currPath.length; mod++) {
+        //When we redraw points. We need a break when we reach the end of an svg path.
+        pFIVE.beginShape();
+        pFIVE.noFill();
+        //Draw all previouse points of drawing for every frame of animation.
+        for (let i = 0; i < currPath[mod].length; i++) {
+          pFIVE.vertex(currPath[mod][i].x, currPath[mod][i].y);
+        }
+        pFIVE.endShape();
+      }
+
+      const dt = pFIVE.TWO_PI / allFourierY[currSVGPath].length;
+      //Should be 2pi a full cycle per frame / the number of fourier coefficents.
+      localTime += dt;
+
+      if (localTime >= pFIVE.TWO_PI) {
+        //Finsihed one svg path completly.
+        time += pFIVE.TWO_PI;
+        localTime = 0;
+        currSVGPath++;
+        currPath.push([]);
+
+        //After we have drawn all paths and been around once, we reset and draw again.
+        if (time >= pFIVE.TWO_PI * allFourierX.length) {
+          time = 0;
+          currPath.length = 0;
+          currPath.push([]);
+          currSVGPath = 0;
+          localTime = 0;
+        }
+      }
+    };
+  };
+  logoP5 = new p5(customSketch);
+}
+
+new p5(userSketch);
+
+new p5(wave_circle_sketch);
