4a9441cf7c1de6c060ca81a3b6099da593f37785
Tony Rosler <tony.rosler246@gmail.com>
2021-01-29 03:37:21 +0100

    all chnages made

--- src/js/fourier.js
+++ src/js/fourier.js
@@ -31,39 +31,39 @@ export function dft(pFIVE, points) {
  * This is the cooley turkey implmentation.
  * @param { Array<Number>} points Array of values of some wave. Must be a power of 2.
  */
-export function realFFT(points) {
-  if (points.length == 0) {
-    return [];
-  }
-  let numPoints = points.length;
+// export function realFFT(points) {
+//   if (points.length == 0) {
+//     return [];
+//   }
+//   let numPoints = points.length;
 
-  //Has to be power of 2.
-  let power = 1;
-  while (power < numPoints) {
-    power *= 2;
-  }
-  if (!(power === numPoints)) {
-    for (let i = 0; i < power - numPoints; i++) {
-      points.push(0);
-    }
-  }
+//   //Has to be power of 2.
+//   let power = 1;
+//   while (power < numPoints) {
+//     power *= 2;
+//   }
+//   if (!(power === numPoints)) {
+//     for (let i = 0; i < power - numPoints; i++) {
+//       points.push(0);
+//     }
+//   }
 
-  numPoints = points.length;
-  const fft = new FFT(points.length);
-  const out = fft.createComplexArray();
-  fft.realTransform(out, points);
-  fft.completeSpectrum(out);
+//   numPoints = points.length;
+//   const fft = new FFT(points.length);
+//   const out = fft.createComplexArray();
+//   fft.realTransform(out, points);
+//   fft.completeSpectrum(out);
 
-  const fftData = [];
-  // We only have to read the first half of this because of symmetry things.
-  //If we want to reduce the clarity i.e. use less fourier coefficents then we reduce numPoints.
-  for (let k = 0; k < numPoints; k++) {
-    const re = out[2 * k] / numPoints;
-    const im = out[2 * k + 1] / numPoints;
-    const freq = k;
-    const amp = 2 * Math.sqrt(re * re + im * im);
-    const phase = Math.atan2(im, re);
-    fftData[k] = { re, im, freq, amp, phase };
-  }
-  return fftData;
-}
+//   const fftData = [];
+//   // We only have to read the first half of this because of symmetry things.
+//   //If we want to reduce the clarity i.e. use less fourier coefficents then we reduce numPoints.
+//   for (let k = 0; k < numPoints; k++) {
+//     const re = out[2 * k] / numPoints;
+//     const im = out[2 * k + 1] / numPoints;
+//     const freq = k;
+//     const amp = 2 * Math.sqrt(re * re + im * im);
+//     const phase = Math.atan2(im, re);
+//     fftData[k] = { re, im, freq, amp, phase };
+//   }
+//   return fftData;
+// }
